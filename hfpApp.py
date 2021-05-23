d2305fd8e8c16c1e86ff986ced7761dbfc338b47
Gold ochim <ochimgold@yahoo.com>
2021-05-23 18:38:54 +0100

    Update hfpApp.py

--- hfpApp.py
+++ hfpApp.py
@@ -94,14 +94,14 @@ def main():
     st.markdown(html_temp, unsafe_allow_html=True)
     
     Age=st.number_input("Age: ", 0.0, 120.0, step=1.0)
-    sex = st.selectbox("Sex: ", ('male', 'female'))
-    cp=st.selectbox("cp: ", ('Typical Angina', 'Atypical Angina', 'Non Angina', 'Asymptomatic') )    
+    sex = st.selectbox("Sex: ", ('1-male', '0-female'))
+    cp=st.selectbox("cp: ", ('1-Typical Angina', '2-Atypical Angina', '3-Non Angina', '4-Asymptomatic') )    
     sc=st.number_input("sc: ", 0.0, 500.0, step=1.0)
-    tstr= st.selectbox("tstr: ",('Normal', 'Fixed Defect', 'Reversible Defect'))
-    rer=st.selectbox("rer: ", ('Normal', 'Having ST Wave Abnormality', 'Showing Probable or Definite Left Ventricular hypertrophy by Estes Criteria'))
+    tstr= st.selectbox("tstr: ",('3-Normal', '6-Fixed Defect', '7-Reversible Defect'))
+    rer=st.selectbox("rer: ", ('0-Normal', '1-Having ST Wave Abnormality', '2-Showing Probable or Definite Left Ventricular hypertrophy by Estes Criteria'))
     nmv=st.number_input("nmv: ",0.0, 3.0, step=1.0)
     rbp=st.number_input("rbp: ", 0.0, 200.0, step=1.0)        
-    spe=st.selectbox("spe: ",('Upsloping', 'Flat', 'Downsloping'))
+    spe=st.selectbox("spe: ",('1-Upsloping', '2-Flat', '3-Downsloping'))
     sdierr=st.number_input("sdierr: ", 0.0, 6.2, step=0.1)
     
     
@@ -113,13 +113,13 @@ def main():
     if st.button("Prediction Note"):
         st.text("This is a thesis work for Gold Ogeyi Ochim from the Federal University of Agriculture Makurdi")
     if st.button("Guide"):
-        st.text("Chest pain type: 1-Typical angina, 2-Atypical anginal, 3-Non anginal, 4-Asymptomatic")
-        st.text("Thalium stress Test Result Value: 3-Normal, 6-fixed defect, 7-Reversible defect" )
-        st.text("Resting Electrocardiographic Result:  0-Normal, 1-Having ST Wave Abnormality, 2-Showing Probable or definite left ventricular hypertrophy by Estes' criteria")
-        st.text("Number of major vessels: 0-3")
-        st.text("Resting blood pressure: 0-200 ")
-        st.text("The slope of the peak exercise ST segment: 1-Upslopping, 2- Flat, 3-Down slopping")
-        st.text("ST depression induced by exercise relative to rest: 0-6.2")       
+        st.text("cp-Chest pain type: 1-Typical angina, 2-Atypical anginal, 3-Non anginal, 4-Asymptomatic")
+        st.text("tstr-Thalium stress Test Result Value: 3-Normal, 6-fixed defect, 7-Reversible defect" )
+        st.text("rer-Resting Electrocardiographic Result:  0-Normal, 1-Having ST Wave Abnormality, 2-Showing Probable or definite left ventricular hypertrophy by Estes' criteria")
+        st.text("nmv-Number of major vessels: 0-3")
+        st.text("rer-Resting blood pressure: 0-200 ")
+        st.text("spe-The slope of the peak exercise ST segment: 1-Upslopping, 2- Flat, 3-Down slopping")
+        st.text("sdierr-ST depression induced by exercise relative to rest: 0-6.2")       
         
         
 if __name__=='__main__':
